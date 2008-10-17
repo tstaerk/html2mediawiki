ee1e45060593814e8c65f27b84b2fac1b5187d67
tstaerk <dev@staerk.de>
2008-10-17 18:31:53 +0000

    finally got a file read

--- main.cpp
+++ main.cpp
@@ -4,30 +4,38 @@
 #include <kdebug.h>
 #include <KMessageBox>
 #include <KUrl>
- 
+#include <QFile> 
+
 int main (int argc, char *argv[])
 {
-    KAboutData aboutData(
-                         "html2mediawiki",
-                         0,
-                         ki18n("html2mediawiki"),
-                         "0.1",
-                         ki18n("Converts html syntax to mediawiki syntax"),
-                         KAboutData::License_GPL,
-                         ki18n("(c) 2008 by Thorsten Staerk"),
-                         ki18n("This is html2mediawiki"),
-                         "http://www.staerk.de/thorsten",
-                         "bugs@staerk.de");
- 
-    KCmdLineArgs::init( argc, argv, &aboutData );
-    KCmdLineOptions options; 
-    options.add("+[file]", ki18n("Document to open")); 
-    KCmdLineArgs::addCmdLineOptions(options); 
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  KAboutData aboutData(
+			"html2mediawiki",
+			0,
+			ki18n("html2mediawiki"),
+			"0.1",
+			ki18n("Converts html syntax to mediawiki syntax"),
+			KAboutData::License_GPL,
+			ki18n("(c) 2008 by Thorsten Staerk"),
+			ki18n("This is html2mediawiki"),
+			"http://www.staerk.de/thorsten",
+			"bugs@staerk.de");
 
-    KApplication app;
-    if (args->count()) 
-    {
-      kDebug() << args->url(0).url();
-    }
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  KCmdLineOptions options; 
+  options.add("+[file]", ki18n("Document to open")); 
+  KCmdLineArgs::addCmdLineOptions(options); 
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+  KApplication app;
+  if (args->count()) 
+  {
+    kDebug() << args->url(0).url();
+    QFile inputfile(args->url(0).fileName());
+    inputfile.open(QIODevice::ReadOnly);
+    kDebug() << inputfile.bytesAvailable();
+    QByteArray inputfilecontent = inputfile.read(inputfile.bytesAvailable());
+    kDebug() << inputfilecontent;
+    QString inputfilecontentqstring(inputfilecontent);
+    kDebug() << inputfilecontentqstring;
+  }
 }
